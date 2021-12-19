d8787db05c35bf98ac8ae0cbdbfe55f1dcb54503
AlexFEIII <1257791263@qq.com>
2021-12-19 16:38:56 +0800

    测试

---
+++ ipynb_to_py.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import json
+import os
+
+ipynb_file_path = os.path.join('08线性回归/')
+ipynb_file_names = [
+    '线性回归',
+]
+
+for name in ipynb_file_names:
+    with open(ipynb_file_path + name + '.ipynb', 'r', encoding='utf-8') as file:
+        txt = ''
+        for line in file.readlines():
+            txt += line
+        txt_json = json.loads(txt)
+        output_txt = ''
+        for cell_txt_json in txt_json['cells']:
+            if cell_txt_json['cell_type'] == 'code':
+                for source_txt in cell_txt_json['source']:
+                    output_txt += source_txt
+            output_txt += '\n'
+        print(output_txt)
+        with open(ipynb_file_path + name + '.py', 'w', encoding='utf-8') as output_file:
+            output_file.write(output_txt)
